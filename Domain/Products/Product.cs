a7363751570cf8a705792aca5f26e3ed6552252f
Marcos García <mgarcia@eshopworld.com>
2024-05-27 17:11:16 +0200

    merge

---
+++ Domain/Products/Product.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Primitives;
+
+namespace Domain.Products
+{
+    public sealed class Product : AggregateRoot
+    {
+        public Guid Id { get; private set; }
+        public ProductName Name { get; private set; }
+        public StockQuantity Stock { get; private set; }
+
+        private Product(Guid id, ProductName name, StockQuantity stock) : base(id)
+        {
+            Id = id;
+            Name = name;
+            Stock = stock;
+        }
+
+        public static Product Create(ProductName name, StockQuantity stock)
+        {
+            return new Product(Guid.NewGuid(), name, stock);
+        }
+    }
+}
---
+++ Domain/Products/StockQuantity.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Products.Exceptions;
+
+namespace Domain.Products
+{
+    public sealed record StockQuantity
+    {
+        public const decimal MinimumValue = 0;
+
+        private StockQuantity(decimal value) => Value = value;
+
+        public decimal Value { get; init; }
+
+        public static StockQuantity Create(decimal value)
+        {
+            if (MinimumValue < value)
+            {
+                throw new StockQuantityIsInvalidException();
+            }
+
+            return new StockQuantity(value);
+        }
+    }
+}
