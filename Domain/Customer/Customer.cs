1bfde545de49eef9d609d5bbc506468950367241
Arturo Castro <arturo.castro@scalefast.com>
2024-05-27 16:16:43 +0200

    First Commit Customer Entity Created

---
+++ Domain/Customer/Customer.cs
@@ -0,0 +1,26 @@
+﻿namespace Domain.Customer
+{
+    public sealed class Customer
+    {
+        public FirstName FirstName { get; set; }
+        public LastName LastName { get; set; }
+        public Email Email { get; set; }
+        
+        public Guid Id { get; private set; }
+
+        private Customer(Guid id, FirstName firstName, LastName lastName, Email email)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public static Customer Create(FirstName firstName, LastName lastname, Email email)
+        {
+            var guid = Guid.NewGuid();
+            var customer = new Customer(guid, firstName, lastname, email);
+            return customer;
+        }
+    }
+}
---
+++ Domain/Customer/FirstName.cs
@@ -0,0 +1,3 @@
+namespace Domain.Customer;
+
+public sealed record FirstName(string value);
