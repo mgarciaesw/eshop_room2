ce407e65e495dff1882f9a55bc466e8511348f9f
Marcos García <mgarcia@eshopworld.com>
2024-05-27 15:48:35 +0200

    initial commit to add Product and ProductName

---
+++ Domain/Product/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Product
+{
+    public sealed class Product
+    {
+        public Guid Id { get; private set; }
+
+        private Product(Guid id)
+        {
+            Id = id;
+        }
+
+        public static Product Create()
+        {
+            return new Product(Guid.NewGuid());
+        }
+    }
+}
---
+++ Domain/Product/ProductName.cs
@@ -0,0 +1,27 @@
+﻿namespace Domain.Product
+{
+    public sealed record ProductName
+    {
+        public const int MaximumLength = 100;
+
+        private ProductName(string value) => Value = value;
+
+        public string Value { get; init; }
+
+        public static ProductName Create(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+
+                throw new Exception();
+            }
+
+            if (MaximumLength < value.Length)
+            {
+                throw new Exception();
+            }
+
+            return new ProductName(value);
+        }
+    }
+}
