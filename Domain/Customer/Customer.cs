0d212b3ff678f8cfdd437b31feba46131477b58b
Arturo Castro <arturo.castro@scalefast.com>
2024-05-27 17:29:39 +0200

    Shipping address added to Customer entity

--- Domain/Customer/Customer.cs
+++ Domain/Customer/Customer.cs
@@ -7,18 +7,20 @@ namespace Domain.Customer
         public FirstName FirstName { get; set; }
         public LastName LastName { get; set; }
         public Email Email { get; set; }
+        public ShippingAddress ShippingAddress { get; set; }
 
-        private Customer(Guid id, FirstName firstName, LastName lastName, Email email) : base(id)
+        private Customer(Guid id, FirstName firstName, LastName lastName, Email email,  ShippingAddress shippingAddress) : base(id)
         {
             FirstName = firstName;
             LastName = lastName;
             Email = email;
+            ShippingAddress = shippingAddress;
         }
 
-        public static Customer Create(FirstName firstName, LastName lastname, Email email)
+        public static Customer Create(FirstName firstName, LastName lastname, Email email, ShippingAddress shippingAddress)
         {
             var guid = Guid.NewGuid();
-            var customer = new Customer(guid, firstName, lastname, email);
+            var customer = new Customer(guid, firstName, lastname, email, shippingAddress);
             return customer;
         }
     }
