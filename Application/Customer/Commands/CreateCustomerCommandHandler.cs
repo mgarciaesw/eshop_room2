d358f5d5bef64b7df298f2aca066a1e20c10abff
amcastro90 <161311649+amcastro90@users.noreply.github.com>
2024-05-28 12:59:30 +0200

    add create customer use case

---
+++ Application/Customer/Commands/CreateCustomerCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Customer;
+
+namespace Application.Customer.Commands;
+
+public sealed class CreateCustomerCommandHandler(ICustomerRepository customerRepository)
+{
+    public async Task Handle(CreateCustomerCommand request, CancellationToken cancellationToken = default)
+    {
+        var customer = Domain.Customer.Customer.Create(
+            FirstName.Create(request.FirstName),
+            LastName.Create(request.LastName),
+            Email.Create(request.Email),
+            ShippingAddress.Create(request.ShippingAddress)
+            );
+
+        await customerRepository.Add(customer);
+    }
+}
+
