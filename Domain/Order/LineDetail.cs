a6ffbe71850edeaa8643e833515480c08d59df67
Alejandro G. Riaño <agutierrez@eshopworld.com>
2024-05-28 09:17:43 +0200

    feat: order detail domain

---
+++ Domain/Order/LineDetail.cs
@@ -0,0 +1,52 @@
+﻿using Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Order;
+
+public class LineDetail
+{
+    public Guid Guid { get; internal set; }
+    public Order Order { get; internal set; }
+    public Guid ProductId { get; internal set; }
+    public Money PriceAtOrder { get; internal set; }
+    public Money TotalLine {  get; internal set; }
+    public Quantity Quantity { get; internal set; }
+
+    private LineDetail(
+        Guid guid,
+        Order order,
+        Guid productId,
+        Money priceAtOrder,
+        Money totalLine,
+        Quantity quantity) 
+    {
+        Guid = guid;
+        Order = order;
+        ProductId = productId;
+        PriceAtOrder = priceAtOrder;
+        TotalLine = totalLine;
+        Quantity = quantity;
+    }
+
+    public static LineDetail CreateLine(
+        Order order, 
+        Guid productId, 
+        Money priceAtOrder, 
+        Money totaLine, 
+        Quantity quantity
+    )
+    {
+        return new LineDetail(
+            Guid.NewGuid(),
+            order,
+            productId,
+            priceAtOrder,
+            totaLine,
+            quantity
+        );
+    }
+}
--- Domain/Order/Order.cs
+++ Domain/Order/Order.cs
@@ -3,25 +3,35 @@ namespace Domain.Order;
 using Domain.Primitives;
 using Domain.Shared;
 
+using System.Collections.Generic;
+
 public class Order: AggregateRoot
 {
     public Guid Guid { get; private set; }
     public Money Amount { get; set; }
     public Status Status { get; set; }
+    public readonly List<LineDetail> _lines = new List<LineDetail>();   
+
+    public List<LineDetail> Lines
+    {
+        get { return _lines; }
+    }
 
     private Order(
         Guid guid,
         Money amount,
-        Status status
+        Status status,
+        List<LineDetail> lines
     ): base( guid )
     {
         Guid = guid;
         Amount = amount;
         Status = status;
+        lines.ForEach( ( line ) => { _lines.Add( line ); } );
     }
 
-    public static Order PlaceOrder(Money amount) 
+    public static Order PlaceOrder(Money amount, List<LineDetail> lines) 
     {
-        return new Order(Guid.NewGuid(), amount, Status.Initial);
+        return new Order(Guid.NewGuid(), amount, Status.Initial, lines);
     }
 }
